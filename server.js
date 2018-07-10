0d29446bfcd96f017538b3c2406a6b871373a7ab
Christopher Danella <cdanella@yhmg.com>
2018-07-10 12:54:14 -0400

    Adds projects route to app

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about', (req, res) =>{
   });
 });
 
+app.get('/projects', (req, res) =>{
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page',
+    projectList: 'My projects:',
+  });
+});
+
 // /bad - send back json with an errorMessage property
 //
 app.get('/bad', (req, res) =>{
